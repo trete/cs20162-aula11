47f6cc552e0734dd2afb75e25c3a13f35f16bf21
trete <flytrete@hotmail.com>
2016-11-23 21:32:02 -0200

    23/11

--- cs20162-aula11/src/main/java/com/trete/cs20162/aula11/Calcular.java
+++ cs20162-aula11/src/main/java/com/trete/cs20162/aula11/Calcular.java
@@ -7,11 +7,35 @@ import com.github.kyriosdata.parser.Lexer;
 import com.github.kyriosdata.parser.Parser;
 import com.github.kyriosdata.parser.Token;
 import java.util.List;
-
+/**
+  *Classe utilizada para realizar as operações matemáticas com os parâmetros
+  *inseridos.
+*/
 public class Calcular {
-    public static float calcula(String expressao) {
-        List<Token> tokens = new Lexer(expressao).tokenize();
-        Parser parser = new Parser(tokens);
-        return parser.expressao().valor();
+    public static void main(String[] args) {
+        Calcular calculo = new Calcular();
+        int saida = Calcular.calcula(args[0]);
+        if(saida == 0) {
+            System.out.println(resultado);
+        }
+
+        System.exit(saida);
+    }
+    private static float resultado;
+    /**
+      *Método que calcula o resultado da expressão inserida.
+     * @param expressao é a expressão que terá o resultado calculado.
+     * @return o resultado da expressão.
+     */
+    public static int calcula(String expressao) {
+        try {
+            Lexer tokens = new Lexer(expressao);
+            Parser parser = new Parser(tokens.tokenize());
+            resultado = parser.expressao().valor();
+            return 0;
+        } catch(Exception ex) {
+            System.out.println("insira uma expresão válida");
+            return 1;
+        }
     }
 }
